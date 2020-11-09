18366e8586ad0acb1935868da8ba26a2b032faba
Matias <matiasalarcon55@hotmail.com>
2020-11-09 20:53:15 -0300

    Actividad 3 Terminada

---
+++ tpFinal_mvc/controller/LogoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class LogoutController
+{
+
+    /**
+     * LogoutController constructor.
+     * @param Render $getRender
+     */
+    public function __construct(\Render $getRender)
+    {
+    }
+}
