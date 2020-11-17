9c8c2aa7133e21ff27595c5b11d4473f3c6d1e59
Matias <matiasalarcon55@hotmail.com>
2020-11-17 15:25:51 -0300

    Check

--- tpFinal_mvc/models/MantenimientoModel.php
+++ tpFinal_mvc/models/MantenimientoModel.php
@@ -25,7 +25,7 @@ class MantenimientoModel
             $sql="SELECT LAST_INSERT_ID()";
             $resultado=$this->database->query($sql);
             $id_service=intval($resultado[0]['LAST_INSERT_ID()']);
-            $sql="INSERT INTO Mantenimiento VALUES (DEFAULT,'$patente','$inicio','$final','$kilometraje','$costo','$cod_taller','$mecanico','$id_service','$repuestos_cambiadoss')";
+            $sql="INSERT INTO Mantenimiento VALUES (DEFAULT,'$patente','$inicio','$final','$kilometraje','$costo','$cod_taller','$mecanico','$id_service','$repuestos_cambiados')";
             return $this->database->execute($sql);
         }
         return false;
