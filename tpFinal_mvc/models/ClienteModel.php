c0c7996ebd5a3b6fb5d06708a0ee7a0698b5e032
Matias <matiasalarcon55@hotmail.com>
2020-11-16 23:55:23 -0300

    Archivos Jesi

---
+++ tpFinal_mvc/models/ClienteModel.php
@@ -0,0 +1,56 @@
+<?php
+/**/
+
+class ClienteModel
+{
+    private $database;
+
+    public function __construct($database)
+    {
+        $this->database = $database;
+    }
+
+    public function getClientes()
+    {
+        $sql = "SELECT * FROM Cliente";
+        return $this->database->query($sql);
+    }
+
+    public function getCliente($cuit)
+    {
+        $sql = "SELECT * FROM Cliente WHERE CUIT='$cuit'";
+        return $this->database->query($sql);
+    }
+
+    public function registrar($datos)
+    {
+        $cuit = $datos['CUIT'];
+        $denominacion = $datos['denominacion'];
+        $direccion = $datos['direccion'];
+        $telefono = $datos['telefono'];
+        $email = $datos['email'];
+        $contacto1 = $datos['contacto1'];
+        $contacto2 = $datos['contacto2'];
+        $sql = "INSERT INTO Cliente VALUES (' $cuit','$denominacion',' $direccion','$telefono','$email','$contacto1','$contacto2')";
+        return $this->database->execute($sql);
+    }
+
+    public function editCliente($datos)
+    {
+        $cuit = $datos['CUIT'];
+        $denominacion = $datos['denominacion'];
+        $direccion = $datos['direccion'];
+        $telefono = $datos['telefono'];
+        $email = $datos['email'];
+        $contacto1 = $datos['contacto1'];
+        $contacto2 = $datos['contacto2'];
+        $sql = "UPDATE Cliente SET CUIT='$cuit', denominacion='$denominacion', direccion='$direccion', telefono='$telefono',email='$email',contacto1='$contacto1',contacto2='$contacto2' WHERE CUIT='$cuit'";
+        return $this->database->execute($sql);
+    }
+
+    public function deleteCliente($cuit)
+    {
+        $sql = "DELETE FROM Cliente WHERE CUIT='$cuit'";
+        return $this->database->execute($sql);
+    }
+}
